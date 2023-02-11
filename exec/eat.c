99da447b81df2a8894f0faacc683ac09804ef5a9
Fatih Sarioglu <fsariogl@z1r11p1.42lyon.fr>
2023-02-11 19:37:59 +0100

    refresh

--- exec/eat.c
+++ exec/eat.c
@@ -6,7 +6,7 @@
 /*   By: fsariogl <fsariogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 15:19:49 by fsariogl          #+#    #+#             */
-/*   Updated: 2023/02/11 19:30:02 by fsariogl         ###   ########.fr       */
+/*   Updated: 2023/02/11 19:36:23 by fsariogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ void	eat(t_philo *philo)
 	while (1)
 	{
 		pthread_mutex_lock(&(*philo).mutex_last_eat);
-		if (new_timestamp((*philo).last_eat) >= (*philo).time_to_eat)
+		if (new_timestamp((*philo).last_eat) >= (*philo).time_to_eat
+			|| (*philo).state == STOP || (*philo).state == DEAD)
 		{
 			pthread_mutex_unlock(&(*philo).mutex_last_eat);
 			break;
--- exec/sleep_time.c
+++ exec/sleep_time.c
@@ -6,7 +6,7 @@
 /*   By: fsariogl <fsariogl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 15:23:53 by fsariogl          #+#    #+#             */
-/*   Updated: 2023/02/11 19:29:23 by fsariogl         ###   ########.fr       */
+/*   Updated: 2023/02/11 19:36:20 by fsariogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ void	sleep_time(t_philo *philo)
 	while (1)
 	{
 		pthread_mutex_lock(&(*philo).mutex_last_eat);
-		if (new_timestamp((*philo).start_sleep) >= (*philo).time_to_sleep)
+		if (new_timestamp((*philo).start_sleep) >= (*philo).time_to_sleep
+			|| (*philo).state == STOP || (*philo).state == DEAD)
 		{
 			pthread_mutex_unlock(&(*philo).mutex_last_eat);
 			break;
